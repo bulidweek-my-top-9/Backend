fc3505f936884fb969a4427438ccfe521e316f0e
Blake Mitchell <mblakeandrew@gmail.com>
2019-07-30 15:31:38 -0700

    mad new folder

--- music/music-model.js
+++ music/music-model.js
@@ -1,9 +1,13 @@
 const db = require("../database/dbConfig.js");
 
 module.exports = {
-  find
+  find,
+  add
 }
 
 function find (){
   return db("musicians");
+}
+function add (artist) {
+  return db("musicians").insert(artist);
 }
