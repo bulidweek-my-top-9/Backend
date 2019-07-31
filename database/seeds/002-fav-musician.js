fb95402b7ead919f06350c0b6fcfa5c95ba472e2
Blake Mitchell <mblakeandrew@gmail.com>
2019-07-31 14:38:58 -0700

    removed a rwo form seed for musicians

--- database/seeds/002-fav-musician.js
+++ database/seeds/002-fav-musician.js
@@ -5,16 +5,16 @@ exports.seed = function(knex) {
   //   .then(function () {
       // Inserts seed entries
       return knex('favoriteMusicians').insert([
-        {id: 1, user_id: '1', artist_id: '1'},
-        {id: 2, user_id: '1', artist_id: '11'},
-        {id: 3, user_id: '1', artist_id: '10'},
-        {id: 4, user_id: '1', artist_id: '21'},
-        {id: 5, user_id: '1', artist_id: '2'},
-        {id: 6, user_id: '1', artist_id: '3'},
-        {id: 7, user_id: '1', artist_id: '4'},
-        {id: 8, user_id: '1', artist_id: '5'},
-        {id: 9, user_id: '1', artist_id: '6'},
-        {id: 10, user_id: '2', artist_id: '19'}
+        {user_id: '1', artist_id: '1'},
+        {user_id: '1', artist_id: '11'},
+        {user_id: '1', artist_id: '10'},
+        {user_id: '1', artist_id: '21'},
+        {user_id: '1', artist_id: '2'},
+        {user_id: '1', artist_id: '3'},
+        {user_id: '1', artist_id: '4'},
+        {user_id: '1', artist_id: '5'},
+        {user_id: '1', artist_id: '6'},
+        {user_id: '2', artist_id: '19'}
       ]);
     // });
 };
