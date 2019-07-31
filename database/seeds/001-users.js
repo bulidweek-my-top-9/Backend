76dbb8885a5ac7f9bbcaa258eb0836f761fb65dc
Blake Mitchell <mblakeandrew@gmail.com>
2019-07-31 14:01:10 -0700

    romved id form seeds

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -6,9 +6,9 @@ exports.seed = function(knex) {
   //   .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 2, username: 'user1', password: password},
-        {id: 1, username: 'user2', password: password},
-        {id: 3, username: 'user3', password: password}
+        {username: 'user1', password: password},
+        {username: 'user2', password: password},
+        {username: 'user3', password: password}
       ]);
     // });
 };
